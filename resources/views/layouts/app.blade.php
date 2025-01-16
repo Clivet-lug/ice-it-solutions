bfcea1b92745801342b625381c887ab23543fdb4
Clivet-lug <clivetlungu@gmail.com>
2025-01-16 16:33:32 +0200

    Fix asset loading with mix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,9 @@
     <script src="//unpkg.com/alpinejs" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
+    {{-- <!-- Keep just one -->
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script> --}}
+
 </head>
 
 <body class="font-sans antialiased">
--- webpack.mix.js
+++ webpack.mix.js
@@ -4,8 +4,13 @@ mix.js("resources/js/app.js", "public/js")
     .postCss("resources/css/app.css", "public/css", [
         require("tailwindcss"),
         require('autoprefixer'),
-    ]);
+    ])
 
-if (mix.inProduction()) {
-    mix.version();
-}
+    // if (mix.inProduction()) {
+    //     mix.version();
+    // }
+
+    .version();
+
+mix.setPublicPath('public');
+
